076f7fd580020ad9617f98dc67437a6346a2bdf3
David Rivero <david.davidsociety.rivero@gmail.com>
2020-11-16 23:15:29 -0300

    Define sample data and image imports

---
+++ Project/Providers/DummyImageProvider.js
@@ -0,0 +1,7 @@
+export const imageSources = {
+  0: require('../Assets/img/oscar-keys.png'),
+  1: require('../Assets/img/aditya-chinchure.png'),
+  2: require('../Assets/img/aditya-chinchure.png'),
+  3: require('../Assets/img/sarthak-navjivan.png'),
+  4: require('../Assets/img/oscar-keys-2.png')
+};
